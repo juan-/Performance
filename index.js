0861ca94c3f108136e32dbe6a918bac275bc26f9
Juan <juan-@users.noreply.github.com>
2018-04-30 12:21:04 -0400

    Added expression

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/*var mov = {0:[],1:[],2:[],3:[]};
+mov[0].name = 'Granham';
+mov[1].name = 'Dunham';
+mov[2].name = 'Limon';
+mov[3].name = 'Duncan';*/
+
+
+//for (i = 1; i <= 60; 1+15*i) {
+//    for (j = 0; j < 15; j++) {
+//        mov[Math.floor(i / 15)].numers.push(i + j);
+//    }
+//}
+
+console.log(mov);
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.send('dance. dance Jonathan.')
+  //res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
