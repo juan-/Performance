9d0127f614629a37ebb14a2869aa5dead99da289
Juan <juan-@users.noreply.github.com>
2018-04-30 12:28:34 -0400

    why no work

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
+
 /*var mov = {0:[],1:[],2:[],3:[]};
 mov[0].name = 'Granham';
 mov[1].name = 'Dunham';
@@ -25,7 +27,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(process.env.PORT || 8080, function(){
+app.listen(port, function(){
   console.log('listening on *:8080');
 });
     
