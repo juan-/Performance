344224b8835a25cb122f91da5882c80418ea9d3e
Juan <juan-@users.noreply.github.com>
2018-04-30 13:17:37 -0400

    test

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ var io = require('socket.io')(http);
 
 var port = process.env.PORT || 8080;
 
+var section = [1, 16, 31, 46];
+
+var d = 6;
+var votes = [];
+var prob = [];
+
+// sim response 
+votes[1] = 2;
+votes[2] = 3;
+votes[3] = 2;
+votes[4] = 3;
+numVotes = votes.reduce((a, b) => a + b, 0);
+console.log(numVotes)
+
+prob[1] = Math.floor(votes[1]/numVotes*d);
+prob[2] = Math.floor(votes[2]/numVotes*d);
+prob[3] = Math.floor(votes[3]/numVotes*d);
+prob[4] = Math.floor(votes[4]/numVotes*d);
+
+
+console.log(prob)
 /*var mov = {0:[],1:[],2:[],3:[]};
 mov[0].name = 'Granham';
 mov[1].name = 'Dunham';
